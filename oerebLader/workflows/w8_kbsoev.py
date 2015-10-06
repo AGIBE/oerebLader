2c92abdf2b8dbc1d6792ae9eac3c0b8499d279c1
Peter Schär <peter.schaer@bve.be.ch>
2015-10-06 16:20:30 +0200

    Gerüst für ersten Workflow erstellt.

---
+++ oerebLader/workflows/w8_kbsoev.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+import oerebLader.scripts
+
+
+oerebLader.scripts.s26_initialize.run()
+oerebLader.scripts.s4_delete_transfer.run()
+oerebLader.scripts.s24_import_transfer_xtf.run()
+oerebLader.scripts.s30_qa_kbsoev.run()
+oerebLader.scripts.s38_qa_transfer.run()
+oerebLader.scripts.s12_finish.run()
