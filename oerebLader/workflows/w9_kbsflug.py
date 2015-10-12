eea0c999c23c3cf4747c0db2b3e92f944a4793ef
Peter Schär <peter.schaer@bve.be.ch>
2015-10-12 09:43:33 +0200

    Eigenes QA-Script für KBSFLUG eingefügt.

--- oerebLader/workflows/w9_kbsflug.py
+++ oerebLader/workflows/w9_kbsflug.py
@@ -4,7 +4,7 @@ import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s4_delete_transfer
 import oerebLader.scripts.s15_import_kbsbund
 import oerebLader.scripts.s24_import_transfer_xtf
-import oerebLader.scripts.s30_qa_kbsoev
+import oerebLader.scripts.s29_qa_kbsflug
 import oerebLader.scripts.s38_qa_transfer
 import oerebLader.scripts.s12_finish
 
@@ -22,7 +22,7 @@ oerebLader.scripts.s26_initialize.run(config, ticketnr)
 oerebLader.scripts.s4_delete_transfer.run(config)
 oerebLader.scripts.s15_import_kbsbund.run(config)
 oerebLader.scripts.s24_import_transfer_xtf.run(config)
-oerebLader.scripts.s30_qa_kbsoev.run(config)
+oerebLader.scripts.s29_qa_kbsflug.run(config)
 oerebLader.scripts.s38_qa_transfer.run(config)
 oerebLader.scripts.s12_finish.run(config)
 
