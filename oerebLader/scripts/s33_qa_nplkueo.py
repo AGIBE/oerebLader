2a0c7f460804179e1b18e95cf6f80871a3e1c745
Peter Schär <peter.schaer@bve.be.ch>
2015-12-10 14:23:41 +0100

    QA für NPL-Geoprodukte eingebaut.

--- oerebLader/scripts/s33_qa_nplkueo.py
+++ oerebLader/scripts/s33_qa_nplkueo.py
@@ -2,9 +2,30 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import logging
 import os
+import datetime
+import sys
+import arcpy
 
 logger = logging.getLogger('oerebLaderLogger')
 
 def run(config):
     logger.info("Script " +  os.path.basename(__file__) + " wird ausgeführt.")
     logger.info("Script " +  os.path.basename(__file__) + " ist beendet.")
+    toolbox = config['GENERAL']['qa_toolbox']
+    logger.info("Folgende Toolbox wird verwendet: " + toolbox)
+    arcpy.AddToolbox(toolbox)             
+                           
+    qa_spec = os.path.join(config['GENERAL']['qa'], "NPLKUEO.qa.xml")
+    logger.info("Die folgende QA-Spez wird verwendet: " + qa_spec)
+    
+    result_dir_name = unicode("qa_NPLKUEO_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+    result_dir = os.path.join(config['LOGGING']['log_directory'], result_dir_name)
+    logger.info("Die Resultate werden ausgegeben in: " + result_dir)
+    
+    result = arcpy.XmlBasedVerificationTool(qa_spec,"NPLKUEO","25000","Geoprodukte " + config['GEODB_WORK']['connection_file'], "No", result_dir, "File Geodatabase")
+    if unicode(result.getOutput(1))== "true":
+        logger.info("QA-Check Geoprodukt NPLKUEO ohne Fehler durchgelaufen.")
+    else:
+        logger.error("QA-Check Geoprodukt NPLKUEO mit Fehlern abgeschlossen.")
+        logger.error("Import wird abgebrochen.")
+        sys.exit()
