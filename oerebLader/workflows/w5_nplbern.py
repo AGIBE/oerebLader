0b5b1c99cfdf9c677b7d8bf9c6b852dfeed8c1d6
Peter Schär <peter.schaer@bve.be.ch>
2016-01-13 08:21:28 +0100

    w5: CheckLegend eingefügt

--- oerebLader/workflows/w5_nplbern.py
+++ oerebLader/workflows/w5_nplbern.py
@@ -8,6 +8,7 @@ import oerebLader.scripts.s18_import_nplbern
 import oerebLader.scripts.s25_import_uzp
 import oerebLader.scripts.s40_update_oerebsta
 import oerebLader.scripts.s10_derive_nplbern_transfer
+import oerebLader.scripts.s43_checkLegend_npl
 import oerebLader.scripts.s31_qa_npl
 import oerebLader.scripts.s39_qa_uzp
 import oerebLader.scripts.s35_qa_oerebsta
@@ -29,6 +30,7 @@ def run(ticketnr):
     # oerebLader.scripts.s25_import_uzp.run(config)
     # oerebLader.scripts.s40_update_oerebsta.run(config)
     oerebLader.scripts.s10_derive_nplbern_transfer.run(config)
+    oerebLader.scripts.s43_checkLegend_npl.run(config)
     oerebLader.scripts.s31_qa_npl.run(config)
     # oerebLader.scripts.s39_qa_uzp.run(config)
     # oerebLader.scripts.s35_qa_oerebsta.run(config)
