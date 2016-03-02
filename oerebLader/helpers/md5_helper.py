05c50dfa1c28ceb66e31931cfe651337d52687db
Peter Schär <peter.schaer@bve.be.ch>
2016-03-02 17:33:10 +0100

    Encoding des MD5-Files detektieren und das MD5-File mit diesem Encoding öffnen.

--- oerebLader/helpers/md5_helper.py
+++ oerebLader/helpers/md5_helper.py
@@ -4,11 +4,21 @@ import tempfile
 import os
 import requests
 import zipfile
+import chardet
+import codecs
+
+def detect_encoding(encoded_filename):
+    with open(encoded_filename, 'r') as encoded_file:
+        detect_result = chardet.detect(encoded_file.read())
+        detected_encoding = detect_result['encoding']
+        
+        return detected_encoding         
 
 def get_md5_from_zip(zip_url):
     #~ Temporaeren Dateinamen bilden
     tempDir = tempfile.mkdtemp()
     tempFile = os.path.join(tempDir, "data.zip")
+    md5_filename = ""
 
     # aus: http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
     r = requests.get(zip_url, stream=True)
@@ -21,9 +31,14 @@ def get_md5_from_zip(zip_url):
         zip_file = zipfile.ZipFile(tempFile, "r")
         for name in zip_file.namelist():
             if name.endswith(".md5.txt"):
-                md5_new = zip_file.read(name)
+                md5_filename = os.path.join(tempDir, name)
+                zip_file.extract(name, tempDir)
         zip_file.close()
     
-    os.remove(tempFile)
+    os.remove(tempFile)    
+    
+    md5_encoding = detect_encoding(md5_filename)
+    with codecs.open(md5_filename, 'r', encoding=md5_encoding) as md5_file:
+        md5_new = md5_file.read()
     
     return md5_new
