7f41434730cd034ef47c05ffd8d08ab0729a9291
Peter Schär <peter.schaer@bve.be.ch>
2016-03-17 17:18:43 +0100

    s4: Tabellennamen aus Config auslesen.

--- oerebLader/scripts/s4_delete_transfer.py
+++ oerebLader/scripts/s4_delete_transfer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
+import oerebLader.helpers.sql_helper
 import logging
 import os
 import cx_Oracle
@@ -8,30 +9,15 @@ logger = logging.getLogger('oerebLaderLogger')
 
 def run(config):
     logger.info("Script " +  os.path.basename(__file__) + " wird ausgeführt.")
-    schema = 'OEREB'
     liefereinheit = config['LIEFEREINHEIT']['id']
-    tables = [
-        {'tablename': 'AMT', 'liefereinheit_field': 'AMT_LIEFEREINHEIT'},
-        {'tablename': 'ARTIKEL', 'liefereinheit_field': 'ART_LIEFEREINHEIT'},
-        {'tablename': 'DARSTELLUNGSDIENST', 'liefereinheit_field': 'DAR_LIEFEREINHEIT'},
-        {'tablename': 'EIGENTUMSBESCHRAENKUNG', 'liefereinheit_field': 'EIB_LIEFEREINHEIT'},
-        {'tablename': 'FLAECHE', 'liefereinheit_field': 'FLA_LIEFEREINHEIT'},
-        {'tablename': 'GRUVER', 'liefereinheit_field': 'GRV_LIEFEREINHEIT'},
-        {'tablename': 'HINDEF', 'liefereinheit_field': 'HID_LIEFEREINHEIT'},
-        {'tablename': 'HINDEFVOR', 'liefereinheit_field': 'HDV_LIEFEREINHEIT'},
-        {'tablename': 'HINVOR', 'liefereinheit_field': 'HIV_LIEFEREINHEIT'},
-        {'tablename': 'HINWEIDOK', 'liefereinheit_field': 'HWD_LIEFEREINHEIT'},
-        {'tablename': 'LINIE', 'liefereinheit_field': 'LIN_LIEFEREINHEIT'},
-        {'tablename': 'PUNKT', 'liefereinheit_field': 'PUN_LIEFEREINHEIT'},
-        {'tablename': 'VORSCHRIFT', 'liefereinheit_field': 'VOR_LIEFEREINHEIT'}
-    ]
-    
-    with cx_Oracle.connect(config['OEREB_WORK']['connection_string']) as conn:
-        cursor = conn.cursor()
-        for table in tables:
-            logger.info("Lösche aus Tabelle " + table['tablename'])
-            sql = "DELETE FROM %s.%s WHERE %s=%s" % (schema, table['tablename'], table['liefereinheit_field'], liefereinheit)
-            logger.info(sql)
-            cursor.execute(sql)
-        
+    oereb_sql = "SELECT EBECODE, FILTER_FIELD, FILTER_TYPE FROM GPR WHERE GPRCODE='OEREB'"
+    oereb_ebenen = oerebLader.helpers.sql_helper.readSQL(config['OEREB_WORK']['connection_string'], oereb_sql)
+    for oereb_ebene in oereb_ebenen:
+        oereb_table = oereb_ebene[0]
+        oereb_liefereinheit_field = oereb_ebene[1]
+        oereb_delete_sql = "DELETE FROM %s WHERE %s = %s" % (oereb_table, oereb_liefereinheit_field, liefereinheit)
+        logger.info("Deleting...")
+        logger.info(oereb_delete_sql)
+        oerebLader.helpers.sql_helper.writeSQL(config['OEREB_WORK']['connection_string'], oereb_delete_sql)
+
     logger.info("Script " +  os.path.basename(__file__) + " ist beendet.")
