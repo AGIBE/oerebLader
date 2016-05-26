f0b130a574d06687b1c25025a3696799bfe5fffd
Peter Schär <peter.schaer@bve.be.ch>
2016-05-26 17:01:05 +0200

    s38: zusätzlicher Check für OEREB2

--- oerebLader/scripts/s38_qa_transfer.py
+++ oerebLader/scripts/s38_qa_transfer.py
@@ -21,10 +21,15 @@ def run(config):
     oerebLader.create_qaspecs.create_qaspecs.run_create_qaspecs(config, liefereinheiten)
                            
     qa_spec = os.path.join(config['GENERAL']['qa'], "OEREB", "OEREB_" + unicode(config['LIEFEREINHEIT']['id']) + ".qa.xml")
-    logger.info("Die folgende QA-Spez wird verwendet: " + qa_spec)
+    qa2_spec = os.path.join(config['GENERAL']['qa'], "OEREB2", "OEREB2_" + unicode(config['LIEFEREINHEIT']['id']) + ".qa.xml")
+    logger.info("Die folgenden QA-Spez werden verwendet: " + qa_spec)
+    logger.info("Die folgenden QA-Spez werden verwendet: " + qa2_spec)
     
     result_dir_name = unicode("qa_OEREB_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
     result_dir = os.path.join(config['LOGGING']['log_directory'], result_dir_name)
+    result2_dir_name = unicode("qa_OEREB2_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+    result2_dir = os.path.join(config['LOGGING']['log_directory'], result2_dir_name)
+
     logger.info("Die Resultate werden ausgegeben in: " + result_dir)
     
     result = arcpy.XmlBasedVerificationTool(qa_spec,"OEREB","25000","OEREBK-Transferstrukturchecker " + config['OEREB_WORK']['connection_file'], "No", result_dir, "File Geodatabase")
@@ -32,5 +37,15 @@ def run(config):
         logger.info("QA-Check Transferstruktur ohne Fehler durchgelaufen.")
     else:
         logger.error("QA-Check Transferstruktur mit Fehlern abgeschlossen.")
+        logger.error("Import wird abgebrochen.")
+        sys.exit()
+
+    logger.info("Die Resultate werden ausgegeben in: " + result2_dir)
+            
+    result2 = arcpy.XmlBasedVerificationTool(qa2_spec,"OEREB2","25000","OEREBK2-Transferstrukturchecker " + config['OEREB2_WORK']['connection_file'], "No", result2_dir, "File Geodatabase")
+    if unicode(result2.getOutput(1))== "true":
+        logger.info("QA-Check Transferstruktur 2 ohne Fehler durchgelaufen.")
+    else:
+        logger.error("QA-Check Transferstruktur 2 mit Fehlern abgeschlossen.")
         logger.error("Import wird abgebrochen.")
         sys.exit()
