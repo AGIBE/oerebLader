1b93b87d1f68694db5720490d84a4b4d6d4ecaee
Peter Schär <peter.schaer@bve.be.ch>
2016-06-09 13:13:39 +0200

    s46: erste Anpassungen

---
+++ oerebLader/scripts/s46_import_nuplwald.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+import oerebLader.helpers.fme_helper
+import sys
+import logging
+import os
+import fmeobjects
+
+logger = logging.getLogger('oerebLaderLogger')
+
+def run(config):
+    logger.info("Script " +  os.path.basename(__file__) + " wird ausgeführt.")
+    fme_script = os.path.splitext(__file__)[0] + ".fmw"
+    fme_logfile = oerebLader.helpers.fme_helper.prepare_fme_log(fme_script, config['LOGGING']['log_directory']) 
+    logger.info("Script " +  fme_script + " wird ausgef�hrt.")
+    logger.info("Das FME-Logfile heisst: " + fme_logfile)
+    runner = fmeobjects.FMEWorkspaceRunner()
+    bfsnr = config['LIEFEREINHEIT']['bfsnr']
+    itf_file = os.path.join(config['LIEFEREINHEIT']['gpr_source'], unicode(bfsnr), unicode(bfsnr) + ".itf")
+    input_rv_dir = os.path.join(config['LIEFEREINHEIT']['gpr_source'], unicode(bfsnr), "rv")
+    output_rv_dir = os.path.join(config['GENERAL']['files_be_ch_baseunc'], unicode(config['LIEFEREINHEIT']['id']), unicode(config['ticketnr']))
+    if config['GENERAL']['files_be_ch_baseurl'].endswith("/"):
+        output_rv_url = config['GENERAL']['files_be_ch_baseurl'] + unicode(config['LIEFEREINHEIT']['id']) + "/" + unicode(config['ticketnr']) + "/"
+        legend_baseurl = config['GENERAL']['files_be_ch_baseurl'] + "legenden/Waldgrenzen/Waldgrenzen.png"
+    else:
+        output_rv_url = config['GENERAL']['files_be_ch_baseurl'] + "/" + unicode(config['LIEFEREINHEIT']['id']) + "/" + unicode(config['ticketnr']) + "/"
+        legend_baseurl = config['GENERAL']['files_be_ch_baseurl'] + "/legenden/Waldgrenzen/Waldgrenzen.png"
+        
+    
+    # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
+    # Daher müssen workspace und parameters umgewandelt werden!
+    parameters = {
+        'GEODB_DATABASE': str(config['GEODB_WORK']['database']),
+        'GEODB_USERNAME': str(config['GEODB_WORK']['username']),
+        'GEODB_PASSWORD': str(config['GEODB_WORK']['password']),
+        'OEREB_DATABASE': str(config['OEREB_WORK']['database']),
+        'OEREB_USERNAME': str(config['OEREB_WORK']['username']),
+        'OEREB_PASSWORD': str(config['OEREB_WORK']['password']),
+        'MODELLABLAGE': str(config['GENERAL']['models']),
+        'BFSNR': str(bfsnr),
+        'GEMNAME': config['LIEFEREINHEIT']['gemeinde_name'].encode("latin-1"),
+        'ITF_FILE': str(itf_file),
+        'INPUT_RV_DIR': str(input_rv_dir),
+        'OUTPUT_RV_DIR': str(output_rv_dir),
+        'OUTPUT_RV_URL': str(output_rv_url),
+        'LEGEND_BASEURL': str(legend_baseurl),
+        'LIEFEREINHEIT': str(config['LIEFEREINHEIT']['id']),
+        'LOGFILE': str(fme_logfile)
+    }
+    try:
+        runner.runWithParameters(str(fme_script), parameters)
+    except fmeobjects.FMEException as ex:
+        logger.error("FME-Workbench " + fme_script + " konnte nicht ausgeführt werden!")
+        logger.error(ex)
+        logger.error("Import wird abgebrochen!")
+        sys.exit()
+        
+    logger.info("Script " +  os.path.basename(__file__) + " ist beendet.")
+    
