5eff99ef2c4712e21a1f498d5838a1adf2099748
Peter Schär <peter.schaer@bve.be.ch>
2016-08-19 13:00:37 +0200

    s51: Workaround für Bundeslegenden angelegt.

---
+++ oerebLader/scripts/s51_create_federallegends.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+import logging
+import datetime
+
+logger = logging.getLogger('oerebLaderLogger')
+
+def run(config):
+    logger.info("Script " +  os.path.basename(__file__) + " wird ausgeführt.")
+
+    logger.info("Script " +  os.path.basename(__file__) + " ist beendet.")
