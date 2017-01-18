c816935e1e5fd9fe8bd5e46419fb28d68900ce5e
Peter Schär <peter.schaer@bve.be.ch>
2017-01-18 15:37:13 +0100

    w5: angepasst auf DM16 Bern

--- oerebLader/workflows/w5_nplbern.py
+++ oerebLader/workflows/w5_nplbern.py
@@ -2,15 +2,12 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s1_delete_npl
-import oerebLader.scripts.s5_delete_uzp
 import oerebLader.scripts.s4_delete_transfer
-import oerebLader.scripts.s18_import_nplbern
-import oerebLader.scripts.s25_import_uzp
-import oerebLader.scripts.s40_update_oerebsta
-import oerebLader.scripts.s10_derive_nplbern_transfer
+import oerebLader.scripts.s52_import_nupla_bern
+import oerebLader.scripts.s17_import_npl
+import oerebLader.scripts.s9_derive_npl_transfer
+import oerebLader.scripts.s50_create_legend
 import oerebLader.scripts.s31_qa_npl
-import oerebLader.scripts.s39_qa_uzp
-import oerebLader.scripts.s35_qa_oerebsta
 import oerebLader.scripts.s38_qa_transfer
 import oerebLader.scripts.s44_checkurl_transfer
 import oerebLader.scripts.s12_finish
@@ -23,15 +20,12 @@ def run(ticketnr):
     
     oerebLader.scripts.s26_initialize.run(config, ticketnr)
     oerebLader.scripts.s1_delete_npl.run(config)
-    oerebLader.scripts.s5_delete_uzp.run(config)
     oerebLader.scripts.s4_delete_transfer.run(config)
-    oerebLader.scripts.s18_import_nplbern.run(config)
-    # oerebLader.scripts.s25_import_uzp.run(config)
-    # oerebLader.scripts.s40_update_oerebsta.run(config)
-    oerebLader.scripts.s10_derive_nplbern_transfer.run(config)
+    oerebLader.scripts.s52_import_nupla_bern(config)
+    oerebLader.scripts.s17_import_npl.run(config)
+    oerebLader.scripts.s9_derive_npl_transfer.run(config)
+    oerebLader.scripts.s50_create_legend.run(config)
     oerebLader.scripts.s31_qa_npl.run(config)
-    # oerebLader.scripts.s39_qa_uzp.run(config)
-    # oerebLader.scripts.s35_qa_oerebsta.run(config)
     oerebLader.scripts.s38_qa_transfer.run(config)
     oerebLader.scripts.s44_checkurl_transfer.run(config)
     oerebLader.scripts.s12_finish.run(config)
