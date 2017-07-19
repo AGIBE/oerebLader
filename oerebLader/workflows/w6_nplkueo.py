4d3393ee9238032d7824f22ef59c4a79a7d870ac
Peter Schaer <peter.schaer@bve.be.ch>
2017-07-19 13:54:40 +0200

    #121: s8_derive_nplkueo_transfer

--- oerebLader/workflows/w6_nplkueo.py
+++ oerebLader/workflows/w6_nplkueo.py
@@ -4,7 +4,6 @@ import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s2_delete_nplkueo
 import oerebLader.scripts.s4_delete_transfer
 import oerebLader.scripts.s20_import_nplkueo
-import oerebLader.scripts.s8_derive_nplkueo_transfer
 import oerebLader.scripts.s47_import_nuplkueo
 import oerebLader.scripts.s33_qa_nplkueo
 import oerebLader.scripts.s38_qa_transfer
@@ -21,7 +20,6 @@ def run(ticketnr):
     oerebLader.scripts.s2_delete_nplkueo.run(config)
     oerebLader.scripts.s4_delete_transfer.run(config)
     oerebLader.scripts.s20_import_nplkueo.run(config)
-    oerebLader.scripts.s8_derive_nplkueo_transfer.run(config)
     oerebLader.scripts.s47_import_nuplkueo.run(config)
     oerebLader.scripts.s33_qa_nplkueo.run(config)
     oerebLader.scripts.s38_qa_transfer.run(config)
