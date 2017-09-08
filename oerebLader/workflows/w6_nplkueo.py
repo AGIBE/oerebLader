b19d3ab63cdaabc9873ac29a2ac1f4bf7bfd9469
Peter Schaer <peter.schaer@bve.be.ch>
2017-09-08 09:35:05 +0200

    #120: s2 umbenannt

--- oerebLader/workflows/w6_nplkueo.py
+++ oerebLader/workflows/w6_nplkueo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 import oerebLader.scripts.s26_initialize
-import oerebLader.scripts.s2_delete_nplkueo
+import oerebLader.scripts.s2_delete_nuplkueo
 import oerebLader.scripts.s4_delete_transfer
 import oerebLader.scripts.s20_import_nplkueo
 import oerebLader.scripts.s47_import_nuplkueo
@@ -17,7 +17,7 @@ def run(ticketnr):
     config = oerebLader.helpers.config.get_config()
     
     oerebLader.scripts.s26_initialize.run(config, ticketnr)
-    oerebLader.scripts.s2_delete_nplkueo.run(config)
+    oerebLader.scripts.s2_delete_nuplkueo.run(config)
     oerebLader.scripts.s4_delete_transfer.run(config)
     oerebLader.scripts.s20_import_nplkueo.run(config)
     oerebLader.scripts.s47_import_nuplkueo.run(config)
