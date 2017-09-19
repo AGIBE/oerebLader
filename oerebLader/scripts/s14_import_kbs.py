5b0417e2b62e3af5628ea88c4ed824e17cfa6fd7
Peter Schaer <peter.schaer@bve.be.ch>
2017-09-19 14:21:46 +0200

    #163: s14 erweitert.

--- oerebLader/scripts/s14_import_kbs.py
+++ oerebLader/scripts/s14_import_kbs.py
@@ -4,6 +4,8 @@ import oerebLader.helpers.sql_helper
 import logging
 import os
 import arcpy
+import fmeobjects
+import sys
 
 logger = logging.getLogger('oerebLaderLogger')
 
@@ -39,5 +41,32 @@ def run(config):
         else:
             logger.info("Anzahl Objekte in Quelle und Ziel identisch!")
         logger.info("Ebene " + ebene_name + " wurde kopiert.")
+        
+    fme_script = os.path.splitext(__file__)[0] + ".fmw"
+    fme_logfile = oerebLader.helpers.fme_helper.prepare_fme_log(fme_script, config['LOGGING']['log_directory']) 
+    logger.info("Script " +  fme_script + " wird ausgeführt.")
+    logger.info("Das FME-Logfile heisst: " + fme_logfile)
+    runner = fmeobjects.FMEWorkspaceRunner()
+
+    # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
+    # Daher müssen workspace und parameters umgewandelt werden!
+    parameters = {
+        'GEODB_WORK': str(config['GEODB_WORK']['connection_file']),
+        'GEODB_PG_DATABASE': str(config['GEODB_WORK_PG']['database']),
+        'GEODB_PG_USERNAME': str(config['GEODB_WORK_PG']['username']),
+        'GEODB_PG_PASSWORD': str(config['GEODB_WORK_PG']['password']),
+        'GEODB_PG_HOST': str(config['GEODB_WORK_PG']['host']),
+        'GEODB_PG_PORT': str(config['GEODB_WORK_PG']['port']),
+        'LOGFILE': str(fme_logfile)
+    }
+    try:
+        runner.runWithParameters(str(fme_script), parameters)
+    except fmeobjects.FMEException as ex:
+        logger.error("FME-Workbench " + fme_script + " konnte nicht ausgeführt werden!")
+        logger.error(ex)
+        logger.error("Import wird abgebrochen!")
+        sys.exit()
+
+
             
     logger.info("Script " +  os.path.basename(__file__) + " ist beendet.")
