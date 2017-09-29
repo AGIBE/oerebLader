9a2313cdba69c7d5c55db825d0d6dc98e0190cc3
Peter Schaer <peter.schaer@bve.be.ch>
2017-09-29 14:38:49 +0200

    #168: switch_bfsnr angepasst (INCLUDE nicht expanden)

--- oerebLader/switch_bfsnr/switch_bfsnr.py
+++ oerebLader/switch_bfsnr/switch_bfsnr.py
@@ -38,7 +38,8 @@ def run_switch_bfsnr(bfsnr):
     repo_dir = os.environ["OEREB_REPO_PRUEF"]
     layers = []
     for layer in config['KOMMUNALE_LAYER']:
-        layers.append(layer['layer'].split(".")[1])
+        layers.append(layer['layer'])
+#         layers.append(layer['layer'].split(".")[1])
 
     search_string = "[[[BFSNR]]]"
     bfsnr = unicode(bfsnr)
@@ -55,18 +56,18 @@ def run_switch_bfsnr(bfsnr):
     logger.info("Filter-String ersetzen mit: " + filter_string)
     template_content = template_content.replace(search_string, filter_string)
     
-    for layer in layers:
-        layer_search_string = "###" + layer + "###"
-        layer_replace_string = 'INCLUDE "nupla/' + bfsnr + '/' + layer + '_' + bfsnr + '.map"'
-        logger.info("Layer-String ersetzen: " + layer_replace_string)
-        template_content = template_content.replace(layer_search_string, layer_replace_string)
+#     for layer in layers:
+#         layer_search_string = "###" + layer + "###"
+#         layer_replace_string = 'INCLUDE "nupla/' + bfsnr + '/' + layer + '_' + bfsnr + '.map"'
+#         logger.info("Layer-String ersetzen: " + layer_replace_string)
+#         template_content = template_content.replace(layer_search_string, layer_replace_string)
     
     logger.info("Gemeinde-Mapfile schreiben: " + mapfile_path)
     with codecs.open(mapfile_path, "w", encoding="utf-8") as mapfile:
         mapfile.write(template_content)
         
     logger.info("Gemeinde-Mapfile wird mit mappyfile geparst: " + mapfile_path)
-    mf_content = mappyfile.load(mapfile_path)
+    mf_content = mappyfile.load(mapfile_path, expand_includes=True)
     
     # Sprachunabhängige Parameter manipulieren
     
@@ -84,8 +85,40 @@ def run_switch_bfsnr(bfsnr):
     # Stufe LAYER
     mf_content = oerebLader.helpers.mapfile_helper.fill_layer_language_metadata(mf_content, "de", config)
     
+    # Gemeindespezifische Mapfiles includen
+    # Damit der Include am Beginn, d.h. vor den kantonalen Symbolen
+    # eingefügt wird, muss er mit mappyfile.insert eingefügt werden.
+    # Da aber ein Einzeiler-Include von mappyfile nicht geparst werden kann
+    # (s. https://github.com/geographika/mappyfile/issues/39), muss eine Fake-
+    # CLASS gebildet werden. 
+    for layer in layers:
+        layername_without_prefix = layer.split(".")[1]
+        include_string = '''
+        CLASS
+            INCLUDE "nupla/%s/%s_%s.map"
+        END
+        ''' % (bfsnr, layername_without_prefix, bfsnr)
+        new_include = mappyfile.loads(include_string, expand_includes=False)
+        mf_layer = mappyfile.find(mf_content['layers'], 'name', layer)
+        mf_layer['classes'].insert(0, new_include)
+        
+    mf_dumped = mappyfile.dumps(mf_content)
+    
+    # Die Fake-CLASS wird im Anschluss im gedumpten String durch das effektive 
+    # Include ersetzt. Es wird vor und nach dem Include ein newline eingefügt,
+    # damit das Include sicher auf einer neuen, eigenen Zeile landet.
+    for layer in layers:
+        layername_without_prefix = layer.split(".")[1]
+        include_string = '''
+        CLASS
+            INCLUDE "nupla/%s/%s_%s.map"
+        END
+        ''' % (bfsnr, layername_without_prefix, bfsnr)
+        include_line = '\nINCLUDE "nupla/%s/%s_%s.map"\n' % (bfsnr, layername_without_prefix, bfsnr)
+        mf_dumped = mf_dumped.replace(include_string, include_line)
+        
     logger.info("Gemeinde-Mapfile schreiben: " + mapfile_path)
     with codecs.open(mapfile_path, "w", encoding="utf-8") as mapfile:
-        mapfile.write(mappyfile.dumps(mf_content))
+        mapfile.write(mf_dumped)
         
     logger.info("BFS-Nummer im Gemeinde-Prüfdienst wurde gewechselt auf: " + bfsnr)
