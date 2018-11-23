71e86a3608f1fb9dbd41abc6b6e9e6df79e23cc3
Cornelia Nussberger <cnussberger@gmail.com>
2018-11-23 15:51:24 +0100

    #1096 und #1095 Korrekturen an qa.py und fmw der Importe GBO und NSG

--- oerebLader/scripts/s65_qa_nsg.py
+++ oerebLader/scripts/s65_qa_nsg.py
@@ -14,18 +14,18 @@ def run(config):
     logger.info("Folgende Toolbox wird verwendet: " + toolbox)
     arcpy.AddToolbox(toolbox)
 
-    qa_spec = os.path.join(config['GENERAL']['qa'], "GGO.qa.xml")
+    qa_spec = os.path.join(config['GENERAL']['qa'], "NSG.qa.xml")
     logger.info("Die folgende QA-Spez wird verwendet: " + qa_spec)
 
-    result_dir_name = unicode("qa_GGO_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+    result_dir_name = unicode("qa_NSG_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
     result_dir = os.path.join(config['LOGGING']['log_directory'], result_dir_name)
     logger.info("Die Resultate werden ausgegeben in: " + result_dir)
 
-    result = arcpy.XmlBasedVerificationTool_ProSuite(qa_spec,"GGO","25000","GEODB " + config['GEODB_WORK']['connection_file'], "No", result_dir, "File Geodatabase")
+    result = arcpy.XmlBasedVerificationTool_ProSuite(qa_spec,"NSG","25000","GEODB " + config['GEODB_WORK']['connection_file'], "No", result_dir, "File Geodatabase")
     if unicode(result.getOutput(1))== "true":
-        logger.info("QA-Check Geoprodukt GGO ohne Fehler durchgelaufen.")
+        logger.info("QA-Check Geoprodukt NSG ohne Fehler durchgelaufen.")
     else:
-        logger.error("QA-Check Geoprodukt GGO mit Fehlern abgeschlossen.")
+        logger.error("QA-Check Geoprodukt NSG mit Fehlern abgeschlossen.")
         logger.error("Import wird abgebrochen.")
         sys.exit()
 
