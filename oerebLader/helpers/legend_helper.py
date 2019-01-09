5ca100502056dff1f7e6759a75a2aae9b406aa7e
Peter Schaer <peter.schaer@bve.be.ch>
2019-01-09 10:31:24 +0100

    #1148 Umbau NPL-Legende (Aufteilung in Themen und Subthemen)

--- oerebLader/helpers/legend_helper.py
+++ oerebLader/helpers/legend_helper.py
@@ -8,9 +8,45 @@ import oerebLader.helpers.sql_helper
 
 logger = logging.getLogger('oerebLaderLogger')
 
+def get_legend_subthemes(connection_string, liefereinheit, theme):
+    subtheme_list = []
+    subtheme_sql = "select distinct eib.STH_ID, sth.STH_NAME_DE, sth.STH_NAME_FR, sth.STH_SORT, the.the_sort from EIGENTUMSBESCHRAENKUNG eib LEFT JOIN SUBTHEMA sth ON eib.STH_ID=sth.STH_ID LEFT JOIN THEMA the ON sth.THE_ID=the.THE_ID where eib_liefereinheit=" + unicode(liefereinheit) + " and sth.THE_ID=" + unicode(theme) + " order by the.the_sort, sth.sth_sort"
 
-def get_legend_entries(connection_string, liefereinheit):
-    legend_sql = "select distinct CAST(eib_aussage_de AS NVARCHAR2(1000)) aussage_de, eib_legendesymbol_de, CAST(eib_aussage_fr AS NVARCHAR2(1000)) aussage_fr, eib_legendesymbol_fr, eib_liefereinheit, eib_sort from EIGENTUMSBESCHRAENKUNG where eib_liefereinheit=" + unicode(liefereinheit) + " order by eib_sort asc, aussage_de asc"
+    subthemes = oerebLader.helpers.sql_helper.readSQL(connection_string, subtheme_sql)
+
+    for subtheme in subthemes:
+        subtheme_dict = {
+            "sth_id": subtheme[0],
+            "sth_name_de": subtheme[1],
+            "sth_name_fr": subtheme[2],
+            "sth_sort": subtheme[3],
+            "legend_entries": get_legend_entries(connection_string, liefereinheit, subtheme[0])
+        }
+        subtheme_list.append(subtheme_dict)
+
+    return subtheme_list
+
+def get_legend_themes(connection_string, liefereinheit):
+    theme_list = []
+    themes_sql = "select distinct sth.THE_ID, the.THE_NAME_DE, the.THE_NAME_FR, the.THE_SORT from EIGENTUMSBESCHRAENKUNG eib LEFT JOIN SUBTHEMA sth ON eib.STH_ID=sth.STH_ID LEFT JOIN THEMA the ON sth.THE_ID=the.THE_ID where eib_liefereinheit=" + unicode(liefereinheit) + " order by the.the_sort asc"
+
+    themes = oerebLader.helpers.sql_helper.readSQL(connection_string, themes_sql)
+
+    for theme in themes:
+        theme_dict = {
+            "the_id": theme[0],
+            "the_name_de": theme[1],
+            "the_name_fr": theme[2],
+            "the_sort": theme[3],
+            "subthemes": get_legend_subthemes(connection_string, liefereinheit, theme[0])
+        }
+
+        theme_list.append(theme_dict)
+
+    return theme_list
+
+def get_legend_entries(connection_string, liefereinheit, subtheme):
+    legend_sql = "select distinct CAST(eib_aussage_de AS NVARCHAR2(1000)) aussage_de, eib_legendesymbol_de, CAST(eib_aussage_fr AS NVARCHAR2(1000)) aussage_fr, eib_legendesymbol_fr, eib_liefereinheit, eib_sort from EIGENTUMSBESCHRAENKUNG eib LEFT JOIN SUBTHEMA sth ON eib.STH_ID=sth.STH_ID LEFT JOIN THEMA the ON sth.THE_ID=the.THE_ID where eib_liefereinheit=" + unicode(liefereinheit) + " and eib.STH_ID=" + unicode(subtheme) + " order by eib_sort asc, aussage_de asc"
     legend_list = []
     
     legends = oerebLader.helpers.sql_helper.readSQL(connection_string, legend_sql)
@@ -49,9 +85,9 @@ def create_legends(legend_dir, gemname, bfsnr, liefereinheit, connection_string,
     komplett_template_filename_de = "legend_komplett_template_de.txt"
     komplett_template_filename_fr = "legend_komplett_template_fr.txt"
     
-    legend_entries = get_legend_entries(connection_string, liefereinheit)
+    themes = get_legend_themes(connection_string, liefereinheit)
     template_vars = { "gemname" : gemname,
-                     "legend_entries" : legend_entries
+                     "themes" : themes
                    }
     # Deutsch
     render_template(template_path, npl_template_filename_de, template_vars, html_npl_filename_de)
