cd5b6ba823607a5e355dfb0fcc72bc398dd4928f
Peter Schaer <peter.schaer@bve.be.ch>
2019-02-08 15:39:15 +0100

    #1397 s64 erweitert.

--- oerebLader/scripts/s64_import_nsg.py
+++ oerebLader/scripts/s64_import_nsg.py
@@ -17,17 +17,24 @@ def run(config):
     runner = fmeobjects.FMEWorkspaceRunner()
     # os.path.dirname wird benötigt, weil in gpr_source die MDB enthalten ist.
     input_rv_dir = os.path.join(os.path.dirname(config['LIEFEREINHEIT']['gpr_source']), "rv")
+    excel_file_teilplaene = os.path.join(os.path.dirname(config['LIEFEREINHEIT']['gpr_source']), "rv", "ListeTeilplaene.xlsx")
     output_rv_dir = os.path.join(config['GENERAL']['files_be_ch_baseunc'], unicode(config['LIEFEREINHEIT']['id']), unicode(config['ticketnr']))
     if config['GENERAL']['files_be_ch_baseurl'].endswith("/"):
         output_rv_url = config['GENERAL']['files_be_ch_baseurl'] + unicode(config['LIEFEREINHEIT']['id']) + "/" + unicode(config['ticketnr']) + "/"
+        output_plan_url = config['GENERAL']['files_be_ch_baseurl'] + unicode(config['LIEFEREINHEIT']['id']) + "/plan/"
     else:
         output_rv_url = config['GENERAL']['files_be_ch_baseurl'] + "/" + unicode(config['LIEFEREINHEIT']['id']) + "/" + unicode(config['ticketnr']) + "/"
+        output_plan_url = config['GENERAL']['files_be_ch_baseurl'] + "/" + unicode(config['LIEFEREINHEIT']['id']) + "/plan/"
     excel_file_amt = os.path.join(config['LIEFEREINHEIT']['ts_source'], "AMT_" + unicode(config['LIEFEREINHEIT']['id']) + ".xlsx")
     excel_file_darstellungsdienst = os.path.join(config['LIEFEREINHEIT']['ts_source'], "DARSTELLUNGSDIENST_" + unicode(config['LIEFEREINHEIT']['id']) + ".xlsx")
     if config['GENERAL']['files_be_ch_baseurl'].endswith("/"):
         legend_baseurl = config['GENERAL']['files_be_ch_baseurl'] + "legenden/NSG/"
     else:
         legend_baseurl = config['GENERAL']['files_be_ch_baseurl'] + "/legenden/NSG/"
+    
+    output_plan_dir = os.path.join(config['GENERAL']['files_be_ch_baseunc'], unicode(config['LIEFEREINHEIT']['id']), "plan")
+    logger.info("Output-Dir Pläne: " + output_plan_dir)
+    logger.info("Output-URL Pläne: " + output_plan_url)
 
     # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
     # Daher müssen workspace und parameters umgewandelt werden!
@@ -42,12 +49,20 @@ def run(config):
         'GEODB_PG_PASSWORD': str(config['GEODB_WORK_PG']['password']),
         'GEODB_PG_HOST': str(config['GEODB_WORK_PG']['host']),
         'GEODB_PG_PORT': str(config['GEODB_WORK_PG']['port']),
+        'OEREB_PG_DATABASE': str(config['OEREB_WORK_PG']['database']),
+        'OEREB_PG_USERNAME': str(config['OEREB_WORK_PG']['username']),
+        'OEREB_PG_PASSWORD': str(config['OEREB_WORK_PG']['password']),
+        'OEREB_PG_HOST': str(config['OEREB_WORK_PG']['host']),
+        'OEREB_PG_PORT': str(config['OEREB_WORK_PG']['port']),
         'FGDB': str(config['LIEFEREINHEIT']['gpr_source']),
         'INPUT_RV_DIR': str(input_rv_dir),
         'OUTPUT_RV_DIR': str(output_rv_dir),
         'OUTPUT_RV_URL': str(output_rv_url),
+        'OUTPUT_PLAN_DIR': str(output_plan_dir),
+        'OUTPUT_PLAN_URL': str(output_plan_url),
         'EXCEL_DARSTELLUNGSDIENST': str(excel_file_darstellungsdienst),
         'EXCEL_AMT': str(excel_file_amt),
+        'EXCEL_TEILPLAENE': str(excel_file_teilplaene),
         'LEGEND_BASEURL': str(legend_baseurl),
         'LIEFEREINHEIT': str(config['LIEFEREINHEIT']['id']),
         'LOGFILE': str(fme_logfile)
