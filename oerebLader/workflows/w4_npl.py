2c405fbaf30fd446f426959b24959257eb906c55
Peter Schaer <peter.schaer@bve.be.ch>
2019-07-25 10:33:39 +0200

    Überflüssiges import-Statement entfernt.

--- oerebLader/workflows/w4_npl.py
+++ oerebLader/workflows/w4_npl.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s1_delete_nupla
 import oerebLader.scripts.s4_delete_transfer
-import oerebLader.scripts.s51_derive_darstellung
 import oerebLader.scripts.s48_import_nupla
 import oerebLader.scripts.s49_derive_legendenbildli
 import oerebLader.scripts.s50_create_legend
