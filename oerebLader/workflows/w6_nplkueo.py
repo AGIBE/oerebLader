358ec051f496249f58d54d18bfe2b54e327e1974
Peter Schaer <peter.schaer@bve.be.ch>
2019-08-30 16:28:35 +0200

    #2458 availability-Tabellen in WALDGRENZEN und KUEOS eingebaut.

--- oerebLader/workflows/w6_nplkueo.py
+++ oerebLader/workflows/w6_nplkueo.py
@@ -4,6 +4,7 @@ import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s2_delete_nuplkueo
 import oerebLader.scripts.s4_delete_transfer
 import oerebLader.scripts.s47_import_nuplkueo
+import oerebLader.scripts.s66_update_availability
 import oerebLader.scripts.s38_qa_transfer
 import oerebLader.scripts.s57_qa_nuplkueo
 import oerebLader.scripts.s44_checkurl_transfer
@@ -19,6 +20,7 @@ def run(ticketnr):
     oerebLader.scripts.s2_delete_nuplkueo.run(config)
     oerebLader.scripts.s4_delete_transfer.run(config)
     oerebLader.scripts.s47_import_nuplkueo.run(config)
+    oerebLader.scripts.s66_update_availability.run(config)
     oerebLader.scripts.s38_qa_transfer.run(config)
     oerebLader.scripts.s57_qa_nuplkueo.run(config)
     oerebLader.scripts.s44_checkurl_transfer.run(config)
--- oerebLader/workflows/w7_nplwald.py
+++ oerebLader/workflows/w7_nplwald.py
@@ -4,6 +4,7 @@ import oerebLader.scripts.s26_initialize
 import oerebLader.scripts.s3_delete_nuplwald
 import oerebLader.scripts.s4_delete_transfer
 import oerebLader.scripts.s46_import_nuplwald
+import oerebLader.scripts.s66_update_availability
 import oerebLader.scripts.s38_qa_transfer
 import oerebLader.scripts.s56_qa_nuplwald
 import oerebLader.scripts.s44_checkurl_transfer
@@ -19,6 +20,7 @@ def run(ticketnr):
     oerebLader.scripts.s3_delete_nuplwald.run(config)
     oerebLader.scripts.s4_delete_transfer.run(config)
     oerebLader.scripts.s46_import_nuplwald.run(config)
+    oerebLader.scripts.s66_update_availability.run(config)
     oerebLader.scripts.s38_qa_transfer.run(config)
     oerebLader.scripts.s56_qa_nuplwald.run(config)
     oerebLader.scripts.s44_checkurl_transfer.run(config)
