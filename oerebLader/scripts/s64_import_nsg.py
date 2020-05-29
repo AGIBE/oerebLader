81aa52ef25aac36f4609bc7ae9ec174463c739fd
Peter Schaer <peter.schaer@be.ch>
2020-05-29 14:42:31 +0200

    #5896: s62_import_nsg angepasst.

--- oerebLader/scripts/s64_import_nsg.py
+++ oerebLader/scripts/s64_import_nsg.py
@@ -25,7 +25,7 @@ def run(config):
     else:
         output_rv_url = config['GENERAL']['files_be_ch_baseurl'] + "/" + unicode(config['LIEFEREINHEIT']['id']) + "/" + unicode(config['ticketnr']) + "/"
         output_plan_url = config['GENERAL']['files_be_ch_baseurl'] + "/" + unicode(config['LIEFEREINHEIT']['id']) + "/plan/"
-    excel_file_amt = os.path.join(config['LIEFEREINHEIT']['ts_source'], "AMT_" + unicode(config['LIEFEREINHEIT']['id']) + ".xlsx")
+    excel_file_amt = config['GENERAL']['amt_tabelle']
     excel_file_darstellungsdienst = os.path.join(config['LIEFEREINHEIT']['ts_source'], "DARSTELLUNGSDIENST_" + unicode(config['LIEFEREINHEIT']['id']) + ".xlsx")
     if config['GENERAL']['files_be_ch_baseurl'].endswith("/"):
         legend_baseurl = config['GENERAL']['files_be_ch_baseurl'] + "legenden/NSG/"
