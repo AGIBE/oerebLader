b58278aefec0bc87eb239dfba478c4ec4aa7ea60
Peter Schaer <peter.schaer@be.ch>
2020-06-18 09:58:10 +0200

    #5896 data_integration für Bundesthemen angepasst (office nicht aus Excel)

--- oerebLader/scripts/s67_update_data_integration.py
+++ oerebLader/scripts/s67_update_data_integration.py
@@ -8,16 +8,28 @@ import oerebLader.helpers.sql_helper
 
 logger = logging.getLogger('oerebLaderLogger')
 
+def get_office_id(liefereinheit, schema, config):
+    if config['LIEFEREINHEIT']['amt_oid'] is None:
+        office_id = "dummy"
+        office_sql = "select distinct office_id from %s.public_law_restriction where liefereinheit=%s limit 1" % (schema, liefereinheit)
+        office_sql_result = oerebLader.helpers.sql_helper.readPSQL(config['OEREB_WORK_PG']['connection_string'], office_sql)
+        if len(office_sql_result) == 1:
+            office_id = office_sql_result[0][0]
+
+        return office_id
+    else:
+        return config['LIEFEREINHEIT']['amt_oid']
+
 def run(config):
     logger.info("Script " +  os.path.basename(__file__) + " wird ausgeführt.")
     liefereinheit = unicode(config['LIEFEREINHEIT']['id'])
     update_date = datetime.datetime.now()
-    office_id = config['LIEFEREINHEIT']['amt_oid']
 
     for schema in config['LIEFEREINHEIT']['schemas']:
         logger.info("Aktualisiere " + schema + ".data_integration")
-        logger.info("Hole Office-ID...")
         id = uuid.uuid4()
+        logger.info("Hole Office-ID...")
+        office_id = get_office_id(liefereinheit, schema, config)
         insert_data_integration_sql = "INSERT INTO %s.data_integration (id, date, office_id, liefereinheit) VALUES ('%s', '%s', '%s', %s)" % (schema, id, update_date, office_id, liefereinheit)
         logger.info(insert_data_integration_sql)
         oerebLader.helpers.sql_helper.writePSQL(config['OEREB_WORK_PG']['connection_string'], insert_data_integration_sql)
