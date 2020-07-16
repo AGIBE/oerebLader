16484b853e687d2ec261485d64d1cb25482b2637
Peter Schaer <peter.schaer@be.ch>
2020-07-16 16:02:44 +0200

    #5915: Umstellung auf direkten AGILib-Import

--- src/oerebLader/release/release.py
+++ src/oerebLader/release/release.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-import AGILib.connection
 import AGILib
 import oerebLader.config
 import oerebLader.logging
